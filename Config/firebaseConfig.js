374f4dbcd221d9ab65bbed3ea60e4ae1afce231c
JoaoVitor-br <amorim.jao3@gmail.com>
2026-05-08 16:55:08 -0300

    .

---
+++ Config/firebaseConfig.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB4kQkwK86RX-5XpLkduZ4xTZNnMNvbHjQ",
+  authDomain: "neo-figure.firebaseapp.com",
+  projectId: "neo-figure",
+  storageBucket: "neo-figure.firebasestorage.app",
+  messagingSenderId: "634258246445",
+  appId: "1:634258246445:web:4059af830bd38333c321a4",
+  measurementId: "G-HN8XF7FVL5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
